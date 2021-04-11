1120dd76c5a32b3a3bcec9a43dc03565b998cb18
Hirona Kobayashi <kobayashihirona@team-lab.com>
2021-04-11 19:11:23 +0900

    Recipe Template Page & Query

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -1,7 +1,25 @@
+import { Link } from "gatsby"
 import React from "react"
+import setupTags from "../utils/setupTags"
 
-const TagsList = () => {
-  return <div>this is tags list</div>
+const TagsList = ({ recipes }) => {
+  const newTags = setupTags(recipes)
+
+  return (
+    <div className_="tag-container">
+      <h4>recipes</h4>
+      <div className="tags-list">
+        {newTags.map((tag, index) => {
+          const [text, value] = tag
+          return (
+            <Link key={index} to={`/${text}`}>
+              {text} ({value})
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default TagsList
